6213281b16b033354375d789d37bff4a2e968253
kathy zheng <visualkathy@gmail.com>
2018-07-16 10:11:32 -0700

    try log

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const MUPPETS = ["Kermit", "Miss Piggy", "Fozzie", "Animal", "Scooter", "Skeeter", "Rowlf", "Gonzo", "Bunsen", "Beaker"]
 
 exports.randomMuppet = (req, res) => {
+  console.log('hello')
   const muppet = MUPPETS[Math.floor(Math.random()*MUPPETS.length)]
   console.log(`Returned another muppet! ${muppet}`)
   res.send(`${muppet}`)

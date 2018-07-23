b84ff9aabecffbd69735bd75953f80f72ae8fa49
kathy zheng <visualkathy@gmail.com>
2018-07-23 14:15:33 -0700

    No more js

--- index.js
+++
@@ -1,8 +0,0 @@
-const pokemon = ["caterpie", "butterfree", "whimsicott", "lapras", "bellsprout", "oddish", "dratini"]
-
-exports.randomPokemon = (req, res) => {
-  console.log('hi')
-  const muppet = pokemon[Math.floor(Math.random()*pokemon.length)]
-  console.log(`Returned another muppet! ${pokemon}`)
-  res.send(`${hi}`)
-}

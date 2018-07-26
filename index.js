8299c7c8c4fbd8dcd9062fd328c1ba02837e9fe5
kathy zheng <visualkathy@gmail.com>
2018-07-26 10:34:02 -0700

    josh

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 exports.showHTML = (req, res) => {	
-  console.log(`farto and farta`)	
+  console.log(`josh`)	
   res.set("Content-Type", "text/html")	
   res.status(200).sendFile('index.html', {root: __dirname })	
 }  
